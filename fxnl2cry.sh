3dc75eba4631bf78781a1992e66a6bd9e61a9198
esmuigors2 <144150580+esmuigors2@users.noreply.github.com>
2023-12-28 16:00:06 +0200

    generalized to env. variable

--- fxnl2cry.sh
+++ fxnl2cry.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-fxfile="$HOME/crydarba/tmpl/fxnls/${1}.fxl"
+[ -z "$CRYVAR_FXLDIR" ] && CRYVAR_FXLDIR="$HOME/crydarba/tmpl/fxnls"
+
+fxfile="$CRYVAR_FXLDIR/${1}.fxl"
 [ ! -f "$fxfile" ] && echo "Sorry, no such functional defined!" && exit 1
 #if [ "$$" -eq "$(exec sh -c 'echo "$PPID"')" ]; then
 #    echo -n '"'; cat "$fxfile" | while read un; do echo -n "${un}#"; done; echo '"'
