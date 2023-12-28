ba002d6ed3795fa6f35d61712f5bb9670c7bf7a6
esmuigors2 <144150580+esmuigors2@users.noreply.github.com>
2023-12-28 20:42:59 +0200

    fxnl2cry.sh: now accepts symlinks for custom functionals

--- fxnl2cry.sh
+++ fxnl2cry.sh
@@ -3,7 +3,7 @@
 [ -z "$CRYVAR_FXLDIR" ] && CRYVAR_FXLDIR="$HOME/crydarba/tmpl/fxnls"
 
 fxfile="$CRYVAR_FXLDIR/${1}.fxl"
-[ ! -f "$fxfile" ] && echo "Sorry, no such functional defined!" && exit 1
+[ ! -f "$fxfile" ] && [ ! -L "$fxfile" ] && echo "Sorry, no such functional defined!" && exit 1
 #if [ "$$" -eq "$(exec sh -c 'echo "$PPID"')" ]; then
 #    echo -n '"'; cat "$fxfile" | while read un; do echo -n "${un}#"; done; echo '"'
 #else
